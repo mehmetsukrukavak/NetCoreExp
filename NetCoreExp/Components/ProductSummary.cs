e4d75e3396ebdaece4540b3e06546f8ea326fcd1
Mehmet Kavak <mehmetsukru.kavak07@gmail.com>
2019-01-11 16:59:03 +0300

    1. Normal ViewComponent Ekleme

---
+++ NetCoreExp/Components/ProductSummary.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreExp.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreExp.Components
+{
+    public class ProductSummary:ViewComponent
+    {
+        private IProductRepository productRepository;
+
+        public ProductSummary(IProductRepository _productRepository)
+        {
+            productRepository = _productRepository;
+        }
+
+        public string Invoke()
+        {
+            return $"{productRepository.Products.Count()} Adet Ürün, {productRepository.Products.Sum(a => a.Price)} TL.";
+        }
+    }
+}
