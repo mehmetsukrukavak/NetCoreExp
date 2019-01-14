d193acc11b1d23a8ae9175032898b6f82b9e9c35
Mehmet Kavak <mehmetsukru.kavak07@gmail.com>
2019-01-14 11:50:56 +0300

    Kullanıcı Ekleme işlemi

--- NetCoreExp/Controllers/AdminController.cs
+++ NetCoreExp/Controllers/AdminController.cs
@@ -2,24 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using NetCoreExp.Models;
 
 namespace NetCoreExp.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: /<controller>/
+        private UserManager<ApplicationUser> userManager;
+        public AdminController(UserManager<ApplicationUser> _userManager)
+        {
+            userManager = _userManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(userManager.Users);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ NetCoreExp/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreExp.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage ="Parola Gerekli")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Adı Gerekli")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "E-Mail Gerekli")]
+        public string Email { get; set; }
+    }
+}
