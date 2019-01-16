2f3bbb2611d77d2ad4b6abc31120ea62b77936a5
msukrukavak <mehmetsukru.kavak07@gmail.com>
2019-01-16 09:51:08 +0300

    RolModel Eklenmesi

---
+++ NetCoreExp/Models/RolModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace NetCoreExp.Models
+{
+    public class RolDetails
+    {
+        public IdentityRole Rol { get; set; }
+
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+
+    public class RolEditModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+
+    }
+}
